942f3ffc0407f72492a6a7099b4195fb7ebc3ee3
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-01 15:30:40 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,3 +1,105 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+// using heaps algorithm to generate permutations of the connections
+// https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
+
+// graph input data structure is adjacency list
+function are_isomorphic (graph1, graph2) 
+{
+    // check if they have same number of nodes
+    if (graph1.length != graph2.length)
+    {
+        return false;
+    }
+
+    // check if already matching
+    for (var i = 0; i < graph1.length; i++)
+    {
+        if(checkMatching(graph1[i], graph2, i))
+        {
+            return true;
+        }
+    }
+
+    // for each node, generate permutations of connections
+    for (var i = 0; i < graph1.length; i++)
+    {
+        if (heapPermutation(graph1, graph2, graph1[i].length, graph1[i].length, node))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
+
+function heapPermutation (array, graph2, size, n, node)
+{
+    if (size == 1)
+    {
+        if (checkMatching(array, graph2, node))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < size; i++) {
+        if(heapPermutation(a, size - 1, n))
+        {
+            return true;
+        }
+
+        if (size % 2 == 1) 
+        {
+            let temp = a[0];
+            a[0] = a[size - 1];
+            a[size - 1] = temp;
+        }
+        else 
+        {
+            let temp = a[i];
+            a[i] = a[size - 1];
+            a[size - 1] = temp;
+        }
+    }
+}
+
+function checkMatching (graph1Node, graph2, node)
+{
+    for (var i = 0; i < graph1Node.length; i++)
+    {
+        if (graph1Node[i] != graph2[node][i])
+        {
+            return false
+        }
+    }
+
+    return true;
+}
+
+// temporary testing code
+// used test graphs from my graph search and dijkstras
+
+testGraph1 = [[1, 5], [0, 2, 3, 4], [1], [1, 4, 6], [1, 3], [0], [3]];
+testGraph2 = [[5, 1], [4, 3, 0, 2], [1], [4, 1, 6], [3, 1], [0], [3]];
+
+testGraph3 = [[1, 2], [0, 2, 3], [0, 1, 3], [1, 2]];
+testGraph4 = [[2, 1], [3, 0, 2], [1, 0, 3], [1, 2]];
+
+// looking for true
+var result1 = are_isomorphic(testGraph1, testGraph2);
+var result2 = are_isomorphic(testGraph3, testGraph4);
+
+// looking for false
+var result3 = are_isomorphic(testGraph1, testGraph4);
+var result4 = are_isomorphic(testGraph2, testGraph3);
+
+// print
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
